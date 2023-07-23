0c554b3b5973f2a2aa393ab7d124c4ea2c7e56a5
ffflukentp <Natthaphonst52@gmail.com>
2023-07-23 13:38:37 +0700

    Update api

--- Model/room2.php
+++ Model/room2.php
@@ -1,22 +1,34 @@
 <?php
-class Room2{
-    //ตัวแปรที่ใช้ในการติดต่อdatabase
+class Room2
+{
+    //ตัวแปรที่จะใช้ติดต่อกับ Database
     private $conn;
-
+    //ตัวแปรที่จะทำงานคู่กับแต่ละฟิวล์หรือคอลัมน์ในตาราง
     public $id;
     public $airValue1;
     public $airValue2;
     public $airValue3;
     public $roomDate;
-    public $roomtime;
-
+    public $roomTime;
+    //ตัวแปรที่จะเก็บข้อมูล เพื่อเอาไว้ใช้งานเฉย ๆ
     public $message;
-
-    public function __construct($db)
+    //คอนสตรักเตอร์ที่จะมีคำสั่งที่ใช้ในการติดต่อกับ database
+    public function __construct($db) // underscroll 2 ครั้ง
     {
+
         $this->conn = $db;
     }
 
-    
+    //ฟังก์ชั่นต่าง ๆ ที่จะทำงานกับ Database ตาม API ที่เราจะทำการสร้างมันขึ้นมา ซึ่งมีมากน้อยแล้วแต่
+    //function get AlltempRoom1 ที่ทำงานกับ api_getAllTempRoom1.php
+    //วัตถุประสงค์ของฟังก์ชั่นนี้จะไปนำเอาอุณหภูมิใน Room1 ที่มีทั้งหมดมา
+    function getAllTempRoom2()
+    {
+        //คำสั่ง SQL /คำสั่ง SQL  :???????? เรียกว่า พารามิเตอร์ที่จะต้องกำหนดข้อมูลให้มัน
 
+        $strSQL = "SELECT * FROM room2_tb";
+        $stmt = $this->conn->prepare($strSQL);
+        $stmt->execute();
+        return $stmt;
+    }
 }
--- Model/user.php
+++ Model/user.php
@@ -1,13 +1,8 @@
 <?php
 class User
 {
-
     //ตัวแปรที่จะใช้ติดต่อกับ Database
     private $conn;
-
-
-
-
     //ตัวแปรที่จะทำงานคู่กับแต่ละฟิวล์หรือคอลัมน์ในตาราง
     public $userId;
     public $userFullname;
@@ -15,75 +10,24 @@ class User
     public $userPassword;
     public $userStatus;
     //ตัวแปรที่จะเก็บข้อมูล เพื่อเอาไว้ใช้งานเฉย ๆ
-
     public $message;
-
-
-
-
     //คอนสตรักเตอร์ที่จะมีคำสั่งที่ใช้ในการติดต่อกับ database
-
     public function __construct($db) // underscroll 2 ครั้ง
-
     {
-
         $this->conn = $db;
     }
-
-
-
 
     //ฟังก์ชั่นต่าง ๆ ที่จะทำงานกับ Database ตาม API ที่เราจะทำการสร้างมันขึ้นมา ซึ่งมีมากน้อยแล้วแต่
 
     //function loginUser ที่ทำงานกับ api_loginuser.php
-
-
-
-
-    function loginUser()
-
-    {
-
-       
-
+    function loginUser(){
         $strSQL = "SELECT * FROM user_tb WHERE userName = :userName and userPassword = :userPassword";
-
-
-
-        
-
         $stmt = $this->conn->prepare($strSQL);
-
-
-
-
-        
-
         $this->userName = htmlspecialchars(strip_tags($this->userName));
-
         $this->userPassword = htmlspecialchars(strip_tags($this->userPassword));
-
-
-
-
-       
-
         $stmt->bindParam(":userName", $this->userName);
-
         $stmt->bindParam(":userPassword", $this->userPassword);
-
-
-
-
-        
-
         $stmt->execute();
-
-
-
-
-       
-
         return $stmt;
     }
 
@@ -92,12 +36,59 @@ class User
     function registerUser()
 
     {
+        //คำสั่ง SQL
+        $strSQL = "INSERT INTO user_tb (userFullname, userName, userPassword, userStatus)";
+        //ต่อบรรทัด เวลายาวไป
+        $strSQL .= "VALUES (:userFullname, :userName, :userPassword, userStatus)";
+        //กำหนด Statement ที่จะทำงานกับคำสั่ง SQL
+
+        $stmt = $this->conn->prepare($strSQL);
+        
+        //ตรวจสอบข้อมูล
+        $this->userFullname = htmlspecialchars(strip_tags($this->userFullname));
+        $this->userName = htmlspecialchars(strip_tags($this->userName));
+        $this->userPassword = htmlspecialchars(strip_tags($this->userPassword));
+        $this->userStatus = 1;
+       
+        //กำหนดข้อมูลให้กับ พารามิเตอร์
+        $stmt->bindParam(":userFullname", $this->userFullname);
+        $stmt->bindParam(":userName", $this->userName);
+        $stmt->bindParam(":userPassword", $this->userPassword);
+        //สั่งให้SQL ทำงาน
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+
     }
 
     //function updateUser ที่ทำงานกับ api_updateUser.php
 
-    function updateUser()
+    function updateUser(){
+        //คำสั่ง SQL
+        $strSQL = "UPDATE user_tb SET userFullname=:userFullname, userName=:userName, userPassword=:userPassword WHERE userid=:userid";
 
-    {
+        
+        //กำหนด Statement ที่จะทำงานกับคำสั่ง SQL
+        $stmt = $this->conn->prepare($strSQL);
+
+        //ตรวจสอบข้อมูล
+        $this->userId= intval(htmlspecialchars(strip_tags($this->userId)));
+        $this->userFullname = htmlspecialchars(strip_tags($this->userFullname));
+        $this->userName = htmlspecialchars(strip_tags($this->userName));
+        $this->userPassword = htmlspecialchars(strip_tags($this->userPassword));
+        
+        //กำหนดข้อมูลให้กับ พารามิเตอร์
+        $stmt->bindParam(":userid", $this->userId);
+        $stmt->bindParam(":userFullname", $this->userFullname);
+        $stmt->bindParam(":userName", $this->userName);
+        $stmt->bindParam(":userPassword", $this->userPassword);
+        //สั่งให้SQL ทำงาน
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
